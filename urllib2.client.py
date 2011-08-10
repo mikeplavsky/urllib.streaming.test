fb1b7c00f85d74b278e23e23605a7886c5c6c2d4
mikeplavsky <Mike.Plavsky@quest.com>
2011-08-10 15:07:46 +0400

    Fixed "tabs mixed with spaces" issue

--- urllib2.client.py
+++ urllib2.client.py
@@ -8,13 +8,13 @@ def _read_chunked(self,amt):
 
       if chunk_left is None:
 
-	  line = self.fp.readline()
-	  i = line.find(';')
-	  if i >= 0:
-	      line = line[:i] # strip chunk-extensions
-	  chunk_left = int(line, 16)
-	  if chunk_left == 0:
-	      break
+          line = self.fp.readline()
+          i = line.find(';')
+          if i >= 0:
+              line = line[:i] # strip chunk-extensions
+          chunk_left = int(line, 16)
+          if chunk_left == 0:
+              break
 
       yield self._safe_read(chunk_left)
       
@@ -27,11 +27,11 @@ def _read_chunked(self,amt):
   while True:
       line = self.fp.readline()
       if not line:
-	  # a vanishingly small number of sites EOF without
-	  # sending the trailer
-	  break
+          # a vanishingly small number of sites EOF without
+          # sending the trailer
+          break
       if line == '\r\n':
-	  break
+          break
 
   # we read everything; close the "file"
   self.close()
